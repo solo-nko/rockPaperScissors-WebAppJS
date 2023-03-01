67d591685fb470e435b4058bfa9bdccf5a82647d
nko <solowingrazgriz@outlook.com>
2023-03-01 10:27:26 +0900

    Additional styling, particularly for buttons

--- app.js
+++ app.js
@@ -16,7 +16,20 @@ const pVictory = document.getElementById("pVictory");
 const cVictory = document.getElementById("cVictory");
 const tieResult = document.getElementById("tie");
 
-const reset = Array.from(document.getElementsByClassName("reset"));
+const choiceButton = Array.from(document.getElementsByClassName("choice-btn"));
+const btnsToReset = Array.from(document.getElementsByClassName("reset-btn"));
+const logToReset = Array.from(document.getElementsByClassName("reset-log"));
+const victoryToReset = Array.from(document.getElementsByClassName("reset-victory"));
+
+const buttonFade = (pressedButton) => {
+	choiceButton.forEach(element => {
+		if (element !== pressedButton) {
+			element.style.textDecoration = "line-through";
+			element.disabled = true;
+		}
+	pressedButton.disabled = true;		
+	});
+}
 
 const getComputerChoice = () => {
 	cChoice.style.display = "block";
@@ -80,29 +93,35 @@ const determineWinner = (userChoice, computerChoice) =>
 }
 
 btnRock.addEventListener("click", function(){
-	document.getElementById("btn-container").style.display = "none";
+	buttonFade(btnRock);
 	determineWinner("rock", getComputerChoice());
 	pChoiceRock.style.display = "block";	
 	playAgain.style.display = "block";
 });
 
 btnPaper.addEventListener("click", function(){
-	document.getElementById("btn-container").style.display = "none";
-	pChoicePaper.style.display = "block";
+	buttonFade(btnPaper);
 	determineWinner("paper", getComputerChoice());
+	pChoicePaper.style.display = "block";	
 	playAgain.style.display = "block";
 });
 
 btnScissors.addEventListener("click", function(){
-	document.getElementById("btn-container").style.display = "none";
-	pChoiceScissors.style.display = "block";
+	buttonFade(btnScissors);
 	determineWinner("scissors", getComputerChoice());
+	pChoiceScissors.style.display = "block";	
 	playAgain.style.display = "block";
 });
 
 playAgain.addEventListener("click", function(){
-	document.getElementById("btn-container").style.display = "flex";
-	reset.forEach(element => {
-		element.style.display = "none";
+	btnsToReset.forEach(element => {
+		element.disabled = false;
+		element.style.textDecoration = "none";
 	});
+	logToReset.forEach(element => {
+		element.style.display = "none";
+	})
+	victoryToReset.forEach(element => {
+		element.style.display = "none";
+	})
 })
